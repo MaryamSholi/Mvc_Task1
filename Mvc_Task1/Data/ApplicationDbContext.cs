9534d2e01c1558c8ed83b78ed9f7f021516be92e
maryam sholi <marymarie905@gmail.com>
2024-05-07 00:19:26 +0300

    Mvc First

---
+++ Mvc_Task1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mvc_Task1.Models;
+
+namespace Mvc_Task1.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Employee> Employees {  get; set; } 
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=.;database=MvcT1;trusted_connection=true;trustServerCertificate=true");
+		}
+	}
+}
